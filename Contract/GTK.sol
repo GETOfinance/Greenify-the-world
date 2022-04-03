6986f92a08c245df71b530af0aeedd5d47a1798f
GETO.finance (GETO) <74251667+GETOfinance@users.noreply.github.com>
2022-04-03 13:09:54 +0300

    .

--- Contract/GTK.sol
+++ Contract/GTK.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GTKToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("GreenToken", "GTK") {
+    constructor(uint256 initialSupply) public ERC20("GreenToken", "GTK") {
         _mint(msg.sender, initialSupply);
     }
 }
